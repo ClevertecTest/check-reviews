f519f773aadb6bb8bcb4ffa1d8973136ae14ac72
Шидловский Игорь Владимирович <IShidlovskiy@alfabank.ru>
2023-01-16 21:46:12 +0300

    feat(*): console.log(error)

--- dist/index.js
+++ dist/index.js
@@ -10938,77 +10938,45 @@ const { request } = __nccwpck_require__(6234);
 const fs = __nccwpck_require__(7147);
 
 const main = async () => {
-    try {
-        const owner = core.getInput('owner', { required: true });
-        const repo = core.getInput('repo', { required: true });
-        const pull_number = core.getInput('pull_number', { required: true });
-        const token = core.getInput('token', { required: true });
-        const url = 'https://training.cleverland.by/pull-request/opened';
-        const path_to_tests_report = 'cypress/report/report.json';
-        const path_to_tests_screenshots = 'cypress/report/screenshots';
-        const minimum_required_result = 80;
+  try {
+    const owner = core.getInput('owner', { required: true });
+    const repo = core.getInput('repo', { required: true });
+    const pull_number = core.getInput('pull_number', { required: true });
+    const token = core.getInput('token', { required: true });
+    const url = 'https://training.cleverland.by/pull-request/reviewed';
 
-        const octokit = new github.getOctokit(token);
+    const octokit = new github.getOctokit(token);
 
-        const { data: pull_request_info } = await octokit.rest.pulls.get({
-            owner,
-            repo,
-            pull_number,
-        });
+    const { data: pull_request_info } = await octokit.rest.pulls.get({
+      owner,
+      repo,
+      pull_number,
+    });
 
-        const { data: tests_report } = await octokit.rest.repos.getContent({
-            owner,
-            repo,
-            path: path_to_tests_report,
-            ref: pull_request_info.head.ref
-        });
+    const { data: reviews } = await octokit.rest.pulls.listReviews({
+      owner,
+      repo,
+      pull_number,
+    });
 
-        const { data: tests_screenshots } = await octokit.rest.repos.getContent({
-            owner,
-            repo,
-            path: path_to_tests_screenshots,
-            ref: pull_request_info.head.ref
-        });
+    const responseCommitsStatuses = reviews.map(({ state }) => state);
 
-        const buff = Buffer.from(tests_report.content, 'base64');
-        const { stats: tests_stats } = JSON.parse(buff.toString('utf-8'));
-        const { tests, failures, passPercent } = tests_stats;
-
-        const createTestsResultMessage = () => {
-            let tests_result_message = '#  Результаты тестов' + '\n' + `Процент пройденных тестов: ${passPercent}%.` + '\n' + `Общее количество тестов: ${tests}.` + '\n' + `Количество непройденных тестов: ${failures}.` + '\n';
-            
-            tests_screenshots.forEach(({ download_url }) => {
-                tests_result_message += `![Скриншот](${download_url})` + '\n';
-            });
-
-            return tests_result_message;
-        };
-
-        await octokit.rest.issues.createComment({
-            owner,
-            repo,
-            issue_number: pull_number,
-            body: createTestsResultMessage(),
-        });
-
-        await request(`POST ${url}`, {
-            data: { 
-                link: pull_request_info.html_url, 
-                github: owner,
-                isTestsSuccess: passPercent >= minimum_required_result
-            },
-            headers: {
-              'Content-Type': 'application/json;charset=utf-8'
-            },
-        });
-
-    } catch (error) {
-        core.setFailed(error.message);
+    if (responseCommitsStatuses.includes('CHANGES_REQUESTED')) {
+      await request(`POST ${url}`, {
+        data: {
+          github: pull_request_info.user.login,
+          isApproved: false,
+        },
+      });
     }
-}
+  } catch (error) {
+    console.log(error);
+    core.setFailed(error.message);
+  }
+};
 
-// Call the main function to run the action
 main();
+
 })();
 
 module.exports = __webpack_exports__;
